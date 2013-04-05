8d4fbf5b5e5c9706f7b7b74ab442809280e989e6
jbrekke <Jeff.Brekke@qg.com>
2013-04-05 14:34:08 -0500

    Shell of the source file

--- W4Assignment1/src/main/java/Program.java
+++ W4Assignment1/src/main/java/Program.java
@@ -1,59 +1,59 @@
 public class Program {
 
-	private static final int TOTAL_ROUNDS = 6;
-	private volatile int rounds = 0;
-	private volatile boolean turn = true;
+    private static final int TOTAL_ROUNDS = 6;
+    private volatile int rounds = 0;
+    private volatile boolean turn = true;
 
-	public synchronized void ping() {
-		try {
-			if (turn) {
-				rounds++;
-				System.out.println("Ping!");
-				turn = false;
-			}
-			wait();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+    public synchronized void ping() {
+        try {
+            if (turn) {
+                rounds++;
+                System.out.println("Ping!");
+                turn = false;
+            }
+            wait();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public synchronized void pong() {
-		if (!turn) {
-			rounds++;
-			System.out.println("Pong!");
-			turn = true;
-		}
-		notify();
-	}
+    public synchronized void pong() {
+        if (!turn) {
+            rounds++;
+            System.out.println("Pong!");
+            turn = true;
+        }
+        notify();
+    }
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 		
-		System.out.print("Ready... ");
-		final Program game = new Program();
+        System.out.print("Ready... ");
+        final Program game = new Program();
 
-		System.out.print("Set... ");
-		Thread ping = new Thread() {
-			public void run() {
-				while (game.rounds < game.TOTAL_ROUNDS)
-					game.ping();
-			}
-		};
-		Thread pong = new Thread() {
-			public void run() {
-				while (game.rounds < game.TOTAL_ROUNDS)
-					game.pong();
-			}
-		};
+        System.out.print("Set... ");
+        Thread ping = new Thread() {
+                public void run() {
+                    while (game.rounds < game.TOTAL_ROUNDS)
+                        game.ping();
+                }
+            };
+        Thread pong = new Thread() {
+                public void run() {
+                    while (game.rounds < game.TOTAL_ROUNDS)
+                        game.pong();
+                }
+            };
 				
-		System.out.println("Go!");
-		System.out.println("");
+        System.out.println("Go!");
+        System.out.println("");
 				
-		ping.start();
-		pong.start();
+        ping.start();
+        pong.start();
 		
-		ping.join();
-		pong.join();
+        ping.join();
+        pong.join();
 		
-		System.out.println("Done!");
-	}
+        System.out.println("Done!");
+    }
 }
---
+++ W4Assignment2/src/main/java/Program.java
@@ -0,0 +1,11 @@
+public class Program {
+
+    public static void main(String[] args) throws Exception {
+		
+	System.out.println("Dinner is starting!");
+
+	//Program game = new Program();
+		
+	System.out.println("Dinner is over!");
+    }
+}
