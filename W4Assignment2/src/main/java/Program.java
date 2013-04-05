d2822841f6875a6efb6469a61a787a50fd9db152
jbrekke <Jeff.Brekke@qg.com>
2013-04-05 14:57:50 -0500

    Started thread for each philosopher.

--- W4Assignment2/src/main/java/Program.java
+++ W4Assignment2/src/main/java/Program.java
@@ -1,11 +1,45 @@
 public class Program {
 
+    private Thread[] guests;
+
+    private class Philosopher extends Thread
+    {
+        private int id = -1;
+        Philosopher(int id)
+        {
+            this.id = id;
+        }
+
+        public void run() {
+            System.out.println(String.format("Philosopher %d",id));
+        }                    
+    };
+    
     public static void main(String[] args) throws Exception {
 		
 	System.out.println("Dinner is starting!");
+        System.out.println("");
+        
+        Program dinner = new Program();
+        dinner.serve(5);       
+        dinner.cleanup();
 
-	//Program game = new Program();
-		
-	System.out.println("Dinner is over!");
+        System.out.println("Dinner is over!");
     }
+
+    private void cleanup() throws Exception
+    {
+        for(int i=0; i<guests.length; i++)
+            guests[i].join();
+    }
+    
+    private void serve(int guestCount) throws Exception
+    {
+        guests = new Thread[guestCount];
+        for( int i=0; i<guestCount; i++ )
+        {
+            guests[i] = new Philosopher(i+1);
+            guests[i].start();
+        }
+    }        
 }
