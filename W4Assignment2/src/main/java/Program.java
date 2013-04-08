915c175dae8172d6850a45e58be57a4ba6224d84
Jeff Brekke <ekkerbj@gmail.com>
2013-04-07 22:10:08 -0500

    Not right... Revert " Without wait() and notify()"
    This reverts commit d15bf7d9d2ddd3547211bd3366d7244a84f0ffde.

--- W4Assignment2/src/main/java/Program.java
+++ W4Assignment2/src/main/java/Program.java
@@ -1,101 +1,97 @@
 public class Program {
 
-    private Philosopher[] guests;
+	private Philosopher[] guests;
 
-    private class Chopstick {
-        boolean inUse = false;
+	private class Chopstick {
+		boolean inUse = false;
 
-        private volatile Object monitor = new Object();
+		synchronized public void pickUpChopstick(int id, String name)
+				throws Exception {
+			if (inUse)
+				wait();
+			System.out.println(String.format(
+					"Philosopher %d picks up %s chopstick.", id, name));
+			inUse = true;
+		}
 
-        public boolean pickUpChopstick(int id, String name) throws Exception {
-            synchronized (monitor) {
-                if (!inUse) {
-                    System.out.println(String.format("Philosopher %d picks up %s chopstick.", id, name));
-                    inUse = true;
-                    return true;
-                }
-                return false;
-            }
-        }
+		synchronized void setDownChopstick(int id, String name) {
+			if (inUse) {
+				System.out.println(String.format(
+						"Philosopher %d sets down %s chopstick.", id, name));
+				inUse = false;
+				notify();
+			}
+		}
+	};
 
-        synchronized void setDownChopstick(int id, String name) {
-            synchronized (monitor) {
-                if (inUse) {
-                    System.out.println(String.format("Philosopher %d sets down %s chopstick.", id, name));
-                    inUse = false;
-                }
-            }
-        }
-    };
+	private class Philosopher extends Thread {
+		private int bitesLeft = 5;
+		private int id = -1;
+		private Chopstick left;
+		private Chopstick right;
 
-    private class Philosopher extends Thread {
-        private int bitesLeft = 5;
-        private int id = -1;
-        private Chopstick left;
-        private Chopstick right;
+		Philosopher(int id, Chopstick left, Chopstick right) {
+			this.id = id;
+			this.left = left;
+			this.right = right;
+		}
 
-        Philosopher(int id, Chopstick left, Chopstick right) {
-            this.id = id;
-            this.left = left;
-            this.right = right;
-        }
+		public Chopstick getRightChopstick()
+		{
+			return right;
+		}
+		
+		public void run() {
+			while (bitesLeft > 0) {
+				try {
+					left.pickUpChopstick(id, "left");
+					right.pickUpChopstick(id, "right");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				System.out.println(String.format("Philosopher %d eats.", id));
+				bitesLeft--;
+				right.setDownChopstick(id, "right");
+				left.setDownChopstick(id, "left");
+			}
+		}
 
-        public Chopstick getRightChopstick() {
-            return right;
-        }
+		public void setLeftChopstick(Chopstick leftChopstick) {
+			this.left = leftChopstick;
+		}
+	};
 
-        public void run() {
-            while (bitesLeft > 0) {
-                try {
-                    if (left.pickUpChopstick(id, "left") && right.pickUpChopstick(id, "right")) {
-                        System.out.println(String.format("Philosopher %d eats.", id));
-                        bitesLeft--;
-                    }
-                    right.setDownChopstick(id, "right");
-                    left.setDownChopstick(id, "left");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+	public static void main(String[] args) throws Exception {
 
-        public void setLeftChopstick(Chopstick leftChopstick) {
-            this.left = leftChopstick;
-        }
-    };
+		System.out.println("Dinner is starting!");
+		System.out.println("");
 
-    public static void main(String[] args) throws Exception {
+		Program dinner = new Program();
+		dinner.serve(5);
+		dinner.cleanup();
 
-        System.out.println("Dinner is starting!");
-        System.out.println("");
+		System.out.println("Dinner is over!");
+	}
 
-        Program dinner = new Program();
-        dinner.serve(5);
-        dinner.cleanup();
+	private void cleanup() throws Exception {
+		for (int i = 0; i < guests.length; i++)
+			guests[i].join();
+	}
 
-        System.out.println("Dinner is over!");
-    }
-
-    private void cleanup() throws Exception {
-        for (int i = 0; i < guests.length; i++)
-            guests[i].join();
-    }
-
-    private void serve(int guestCount) throws Exception {
-        guests = new Philosopher[guestCount];
-        Chopstick left = new Chopstick();
-        for (int i = 0; i < guestCount; i++) {
-            Chopstick right = new Chopstick();
-            guests[i] = new Philosopher(i + 1, left, right);
-            left = right;
-        }
-        // Setup the last philosopher's left chopstick with the first one's
-        // right chopstick
-        int lastGuest = guestCount - 1;
-        int firstGuest = 0;
-        guests[lastGuest].setLeftChopstick(guests[firstGuest].getRightChopstick());
-
-        for (int i = 0; i < guestCount; i++)
-            guests[i].start();
-    }
+	private void serve(int guestCount) throws Exception {
+		guests = new Philosopher[guestCount];
+		Chopstick left = new Chopstick();
+		for (int i = 0; i < guestCount; i++) {
+			Chopstick right = new Chopstick();
+			guests[i] = new Philosopher(i + 1, left, right);
+			left = right;
+		}
+		//Setup the last philosopher's left chopstick with the first one's right chopstick
+		int lastGuest = guestCount-1;
+		int firstGuest = 0;		
+		guests[lastGuest].setLeftChopstick ( guests[firstGuest].getRightChopstick() );
+		
+		for (int i=0; i< guestCount; i++)
+			guests[i].start();
+	}
 }
