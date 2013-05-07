38dd55dcc7a3679f533b0585f87a6afbf2fddd39
Jeff Brekke <ekkerbj@gmail.com>
2013-05-06 21:57:31 -0500

    Week 7 done

--- W7Assignment4/src/main/java/Program.java
+++ W7Assignment4/src/main/java/Program.java
@@ -1,4 +1,5 @@
 import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
 
 import org.jboss.netty.bootstrap.ServerBootstrap;
 import org.jboss.netty.channel.ChannelHandlerContext;
@@ -9,20 +10,34 @@ import org.jboss.netty.channel.Channels;
 import org.jboss.netty.channel.MessageEvent;
 import org.jboss.netty.channel.SimpleChannelUpstreamHandler;
 import org.jboss.netty.channel.socket.ServerSocketChannelFactory;
-import org.jboss.netty.channel.socket.oio.OioServerSocketChannelFactory;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
 import org.jboss.netty.handler.codec.frame.DelimiterBasedFrameDecoder;
 import org.jboss.netty.handler.codec.frame.Delimiters;
 import org.jboss.netty.handler.codec.string.StringDecoder;
 import org.jboss.netty.handler.codec.string.StringEncoder;
+import org.jboss.netty.handler.execution.ExecutionHandler;
+import org.jboss.netty.handler.execution.OrderedMemoryAwareThreadPoolExecutor;
 
 public class Program {
+
+    private static final int threadPoolSize = 8;
+    // This is the half-sync portion of the the pattern. Making this execution
+    // handler static ensures there is only one.
+    private static final ExecutionHandler halfSynch = new ExecutionHandler(
+            new OrderedMemoryAwareThreadPoolExecutor(threadPoolSize, 1000000, 10000000));
 
     private final class EchoServerPipelineFactory implements ChannelPipelineFactory {
         public ChannelPipeline getPipeline() throws Exception {
             ChannelPipeline p = Channels.pipeline();
+
             p.addLast("framer", new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter()));
             p.addLast("decoder", new StringDecoder());
             p.addLast("encoder", new StringEncoder());
+
+            // *** This is the half-sync portion, queing up the requests
+            p.addLast("ExecutionHandler", halfSynch);
+            // *** All other handlers are executed with a pool of threads, this
+            // is the half-async portion
             p.addLast("echo", new EchoServerHandler());
             return p;
         }
@@ -62,7 +77,8 @@ public class Program {
 
         // Create the channel factory, acting as part of the acceptor from
         // acceptor/connector.
-        ServerSocketChannelFactory acceptorFactory = new OioServerSocketChannelFactory();
+        ServerSocketChannelFactory acceptorFactory = new NioServerSocketChannelFactory(Executors.newCachedThreadPool(),
+                Executors.newCachedThreadPool());
         ServerBootstrap server = new ServerBootstrap(acceptorFactory);
 
         // The pipelines string together handlers, we set a factory to create
@@ -78,7 +94,7 @@ public class Program {
 
         int port = (args.length > 1) ? Integer.valueOf(args[0]) : 8081;
 
-        System.out.println(" Week 6 Assignment 3: Echo Server w/Netty ");
+        System.out.println(" Week 7 Assignment 4: Echo Server w/Netty using half-sync/half-async");
         System.out.println(" Using port " + port);
         System.out.println(" Ctrl-C to QUIT");
         System.out.println("------------------------------------------");
